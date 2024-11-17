1f438054f5bdf59985982b95701edd0895d18fee
alexlearn02 <alexandre.decourcelle@gmail.com>
2024-11-17 21:24:36 +0100

    jf

--- parking/src/controllers/HomeController.ts
+++ parking/src/controllers/HomeController.ts
@@ -1,5 +1,6 @@
 import {createFactory } from 'hono/factory';
 import { logger } from 'hono/logger';
+import { html } from 'hono/html';
 
 const factory = createFactory();
 const middleware = factory.createMiddleware(async (c, next) => {
@@ -8,7 +9,25 @@ const middleware = factory.createMiddleware(async (c, next) => {
 });
 
 const HelloController = factory.createHandlers(logger(), middleware, (c) => {
-    return c.json(c.var.foo)
+    return c.html(
+        html`<!doctype html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">  
+            <meta name="viewport" content="width=device-width,
+            initial-scale=1.0">
+            <h1> Welcome to EuroPark </h1>
+        </head>
+        <body>
+            <img src="/static/parking.png">
+            <p>"Save time and money with EuroPark! Enjoy 
+            a 100% contactless parking experience for 
+            a short or long duration in our car parks in Europe! </p>
+            <span><a href="/cities">Our Cities</a></span>
+            <span><a href="/parkings">Our Car Parks</a></span>
+        </body>
+        </html>`
+    )
   });
 
 export default HelloController;
--- parking/src/index.ts
+++ parking/src/index.ts
@@ -1,7 +1,10 @@
 import { Hono } from "hono";
-
 import HomeController from "./controllers/HomeController";
+import { serveStatic } from 'hono/bun';
+
 const app = new Hono();
+
+app.use('/static/*', serveStatic({ root: './' }));
 app.get("/", ...HomeController);
 export default app;
 
